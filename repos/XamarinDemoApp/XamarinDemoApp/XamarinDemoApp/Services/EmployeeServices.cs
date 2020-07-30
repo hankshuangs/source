09e1be6a1ae0b28a4d78ec3aded7db30aff8af81
hankshuangs <hankshuangs@gmail.com>
2020-07-31 00:43:12 +0800

    建立XamarinDemoApp(MVVM)

---
+++ repos/XamarinDemoApp/XamarinDemoApp/XamarinDemoApp/Services/EmployeeServices.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamarinDemoApp.Models;
+
+namespace XamarinDemoApp.Services
+{
+    public class EmployeeServices
+    {
+
+        public List<Employee> GetEmployees()
+        {
+            var list = new List<Employee>
+            {
+                new Employee
+                {
+                    Name = "Mohamed",
+                    Department = "Marketing"
+                },
+                new Employee
+                {
+                    Name = "Hassen",
+                    Department = "Sales"
+                },
+                new Employee
+                {
+                    Name = "Ahmed",
+                    Department = "Finance"
+                },
+                new Employee
+                {
+                    Name = "Nader",
+                    Department = "Sales"
+                },
+                new Employee
+                {
+                    Name = "Mohamed",
+                    Department = "HR"
+                },
+                new Employee
+                {
+                    Name = "Saif",
+                    Department = "Sales"
+                },
+                new Employee
+                {
+                    Name = "Mohamed",
+                    Department = "Marketing"
+                },
+                new Employee
+                {
+                    Name = "Marwen",
+                    Department = "HR"
+                },
+            };
+
+            return list;
+        }
+    }
+}
---
+++ repos/XamarinDemoApp/XamarinDemoApp/XamarinDemoApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using XamarinDemoApp.Models;
+using XamarinDemoApp.Services;
+
+namespace XamarinDemoApp.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private List<Employee> _employeesList;
+
+        public List<Employee> EmployeesList 
+        { 
+            get => _employeesList;
+            set {
+                    _employeesList = value;
+                    OnPropertyChanged();
+                } 
+        }
+
+        public MainViewModel()
+        {
+            var employeeServices = new EmployeeServices();
+
+            EmployeesList = employeeServices.GetEmployees();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
