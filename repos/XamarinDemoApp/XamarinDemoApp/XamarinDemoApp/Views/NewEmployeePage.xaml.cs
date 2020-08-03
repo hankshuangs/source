1e3a14ab9bec408383b9f0befc9b61b203c157c9
hankshuangs <hankshuangs@gmail.com>
2020-08-04 01:59:49 +0800

    NewEmployeePage

---
+++ repos/XamarinDemoApp/XamarinDemoApp/XamarinDemoApp/Views/NewEmployeePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinDemoApp.ViewModels;
+
+namespace XamarinDemoApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewEmployeePage : ContentPage
+    {
+        public NewEmployeePage()
+        {
+            InitializeComponent();
+        }
+        public NewEmployeePage(MainViewModel mainViewModel)
+        {
+            InitializeComponent();
+            BindingContext = mainViewModel;
+        }
+    }
+}
